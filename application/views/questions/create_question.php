59e1ebf35617ec391ae03aedc847e7ced8780b61
Filip Rak <filiprak@wp.pl>
2018-08-26 01:08:38 +0200

    Adjust create question form.

--- application/views/questions/create_question.php
+++ application/views/questions/create_question.php
@@ -23,55 +23,115 @@
     <h4 class="ui dividing header">Answers</h4>
 
     <div class="ui segment">
-        <div class="field">
+        <div class="required field">
             <label>Answer 1</label>
             <?php echo form_input($answer1);?>
         </div>
         <div class="five fields">
-            <div class="field"><?php echo form_input($answer1_score_a, '', 'placeholder="Score A"');?></div>
-            <div class="field"><?php echo form_input($answer1_score_i, '', 'placeholder="Score I"');?></div>
-            <div class="field"><?php echo form_input($answer1_score_c, '', 'placeholder="Score C"');?></div>
-            <div class="field"><?php echo form_input($answer1_score_p, '', 'placeholder="Score P"');?></div>
-            <div class="twelve wide field"><?php echo form_input($answer1_next_group, '', 'placeholder="Next Question Group"');?></div>
+            <div class="field">
+                <label>Score A</label>
+                <?php echo form_input($answer1_score_a, '', 'placeholder="Score A"');?>
+            </div>
+            <div class="field">
+                <label>Score I</label>
+                <?php echo form_input($answer1_score_i, '', 'placeholder="Score I"');?>
+            </div>
+            <div class="field">
+                <label>Score C</label>
+                <?php echo form_input($answer1_score_c, '', 'placeholder="Score C"');?>
+            </div>
+            <div class="field">
+                <label>Score P</label>
+                <?php echo form_input($answer1_score_p, '', 'placeholder="Score P"');?>
+            </div>
+            <div class="twelve wide required field">
+                <label>Next Question Group</label>
+                <?php echo form_input($answer1_next_group, '', 'placeholder="Next Question Group"');?>
+            </div>
         </div>
     </div>
     <div class="ui segment">
-        <div class="field">
+        <div class="required field">
             <label>Answer 2</label>
             <?php echo form_input($answer2);?>
         </div>
         <div class="five fields">
-            <div class="field"><?php echo form_input($answer2_score_a, '', 'placeholder="Score A"');?></div>
-            <div class="field"><?php echo form_input($answer2_score_i, '', 'placeholder="Score I"');?></div>
-            <div class="field"><?php echo form_input($answer2_score_c, '', 'placeholder="Score C"');?></div>
-            <div class="field"><?php echo form_input($answer2_score_p, '', 'placeholder="Score P"');?></div>
-            <div class="twelve wide field"><?php echo form_input($answer2_next_group, '', 'placeholder="Next Question Group"');?></div>
+            <div class="field">
+                <label>Score A</label>
+                <?php echo form_input($answer2_score_a, '', 'placeholder="Score A"');?>
+            </div>
+            <div class="field">
+                <label>Score I</label>
+                <?php echo form_input($answer2_score_i, '', 'placeholder="Score I"');?>
+            </div>
+            <div class="field">
+                <label>Score C</label>
+                <?php echo form_input($answer2_score_c, '', 'placeholder="Score C"');?>
+            </div>
+            <div class="field">
+                <label>Score P</label>
+                <?php echo form_input($answer2_score_p, '', 'placeholder="Score P"');?>
+            </div>
+            <div class="twelve wide required field">
+                <label>Next Question Group</label>
+                <?php echo form_input($answer2_next_group, '', 'placeholder="Next Question Group"');?>
+            </div>
         </div>
     </div>
     <div class="ui segment">
-        <div class="field">
+        <div class="required field">
             <label>Answer 3</label>
             <?php echo form_input($answer3);?>
         </div>
         <div class="five fields">
-            <div class="field"><?php echo form_input($answer3_score_a, '', 'placeholder="Score A"');?></div>
-            <div class="field"><?php echo form_input($answer3_score_i, '', 'placeholder="Score I"');?></div>
-            <div class="field"><?php echo form_input($answer3_score_c, '', 'placeholder="Score C"');?></div>
-            <div class="field"><?php echo form_input($answer3_score_p, '', 'placeholder="Score P"');?></div>
-            <div class="twelve wide field"><?php echo form_input($answer3_next_group, '', 'placeholder="Next Question Group"');?></div>
+            <div class="field">
+                <label>Score A</label>
+                <?php echo form_input($answer3_score_a, '', 'placeholder="Score A"');?>
+            </div>
+            <div class="field">
+                <label>Score I</label>
+                <?php echo form_input($answer3_score_i, '', 'placeholder="Score I"');?>
+            </div>
+            <div class="field">
+                <label>Score C</label>
+                <?php echo form_input($answer3_score_c, '', 'placeholder="Score C"');?>
+            </div>
+            <div class="field">
+                <label>Score P</label>
+                <?php echo form_input($answer3_score_p, '', 'placeholder="Score P"');?>
+            </div>
+            <div class="twelve wide required field">
+                <label>Next Question Group</label>
+                <?php echo form_input($answer3_next_group, '', 'placeholder="Next Question Group"');?>
+            </div>
         </div>
     </div>
     <div class="ui segment">
-        <div class="field">
+        <div class="required field">
             <label>Answer 4</label>
             <?php echo form_input($answer4);?>
         </div>
         <div class="five fields">
-            <div class="field"><?php echo form_input($answer4_score_a, '', 'placeholder="Score A"');?></div>
-            <div class="field"><?php echo form_input($answer4_score_i, '', 'placeholder="Score I"');?></div>
-            <div class="field"><?php echo form_input($answer4_score_c, '', 'placeholder="Score C"');?></div>
-            <div class="field"><?php echo form_input($answer4_score_p, '', 'placeholder="Score P"');?></div>
-            <div class="twelve wide field"><?php echo form_input($answer4_next_group, '', 'placeholder="Next Question Group"');?></div>
+            <div class="field">
+                <label>Score A</label>
+                <?php echo form_input($answer4_score_a, '', 'placeholder="Score A"');?>
+            </div>
+            <div class="field">
+                <label>Score I</label>
+                <?php echo form_input($answer4_score_i, '', 'placeholder="Score I"');?>
+            </div>
+            <div class="field">
+                <label>Score C</label>
+                <?php echo form_input($answer4_score_c, '', 'placeholder="Score C"');?>
+            </div>
+            <div class="field">
+                <label>Score P</label>
+                <?php echo form_input($answer4_score_p, '', 'placeholder="Score P"');?>
+            </div>
+            <div class="twelve wide required field">
+                <label>Next Question Group</label>
+                <?php echo form_input($answer4_next_group, '', 'placeholder="Next Question Group"');?>
+            </div>
         </div>
     </div>
 
